99c71503686b04cd5e5e4551c886abf5e77919ba
YertofeX <szabodaniel35@gmail.com>
2023-02-08 17:28:51 +0100

    Basic setup of reactflow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,20 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-import Button from "@/components/Button";
+import Flow from "@/components/Flow";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
+    <div className='h-full flex flex-col'>
       <Head>
         <title>ELTE Subject Explorer</title>
-        <meta name='description' content='TODO' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='description' content='ELTE SUBJECT EXPLORER' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='p-9 flex flex-nowrap gap-2 flex-col'>
-        <Button text='Hello' />
-        <Button text='OK' />
-        <Button text='NO' />
-        <Button text='Cancel' />
-      </div>
-    </>
+
+      <header className='h-10 flex items-center'>ELTE SUBJECT EXPLORER</header>
+      <Flow />
+    </div>
   );
 }
