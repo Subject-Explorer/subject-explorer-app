c683c7bd57e50f32f36af2e373035a3dcc0d436a
Bence Boér <bence0822@gmail.com>
2023-02-27 17:58:12 +0100

    remove unused functions

--- data/csv_to_json.py
+++ data/csv_to_json.py
@@ -1,53 +1,7 @@
-import itertools
 import json
-from itertools import permutations
 
 
-def closest_permutation(semesters):
-    # subject dictionary with id as key
-    subjects = {}
-    for semester in semesters:
-        for subject in semester:
-            subjects[subject["id"]] = subject
-
-    def distance_function(subject, child):
-        # find index of subject and child in semesters
-        subject_index = next(i for i, semester in enumerate(semesters) if subject in semester)
-        child_index = next(i for i, semester in enumerate(semesters) if child in semester)
-
-        return abs(subject_index - child_index)
-
-    def get_distance(subject):
-        if subject is None:
-            return 0
-
-        children_ids = subject["children"]
-
-        # if no children, return 0
-        if not children_ids:
-            return 0
-
-        # if children, return sum of distances, if children are present
-        children = [subjects[child_id] for child_id in children_ids]
-        return sum(distance_function(subject, child) for child in children)
-
-
-    # find longest semester, fill others with None
-    max_semester_length = max(len(semester) for semester in semesters)
-    semesters = [semester + [None] * (max_semester_length - len(semester)) for semester in semesters]
-
-    min_distance = float('inf')
-    min_permutation = None
-    for permutation in permutations(semesters):
-        distance = sum(get_distance(subject) for semester in permutation for subject in semester)
-        if distance < min_distance:
-            min_distance = distance
-            min_permutation = permutation
-
-    return min_permutation
-
-
-def parse_csv(file_name, specialization, table_type) -> list[dict]:
+def parse_csv(file_name, table_type) -> list[dict]:
     subjects = []
 
     with open(f"./csv/{file_name}", mode="r", encoding="utf-8") as file:
@@ -150,25 +104,6 @@ def get_specializations(line, table_type) -> list[str]:
     return specializations
 
 
-def fix_prerequisites(data, spec) -> None:
-    subjects_dict = {}
-
-    # create a dictionary of subjects, with their IDs as keys
-    for semester in data:
-        for subject in semester:
-            subject_id = subject["id"].split('_')[0]
-            subjects_dict[subject_id] = subject
-
-    # replace prerequisite IDs with subject codes
-    for semester in data:
-        for subject in semester:
-            for prerequisite in subject["prerequisites"]:
-                prerequisite_id = prerequisite["id"]
-                if prerequisite_id in subjects_dict:
-                    prerequisite_subject = subjects_dict[prerequisite_id]
-                    prerequisite["id"] = prerequisite_subject["code"] + "_" + spec
-
-
 def map_parents_as_children(subject_data_list):
     # Create a dictionary to map each subject data ID to its children IDs and siblings' IDs.
     child_map = {}
@@ -201,54 +136,16 @@ def sort_into_semesters(subject_data_list) -> list[list[dict]]:
     return data
 
 
-def get_depth(subject_id, prerequisites_dict) -> int:
-    # base case: subject has no prerequisites
-    if subject_id not in prerequisites_dict:
-        return 0
-
-    # recursive case: return depth of deepest prerequisite plus one
-    depths = []
-    for prerequisite_id in prerequisites_dict[subject_id]:
-        depths.append(get_depth(prerequisite_id, prerequisites_dict))
-    return max(depths) + 1
-
-
 def sort_data(data) -> None:
     for semester in data:
         semester.sort(key=lambda x: (x["code"], [prereq["id"] for prereq in x["prerequisites"]], x["field"]),
                       reverse=False)
 
 
-def brute_force_sort(data) -> None:
-    # create a dictionary of subjects, with their IDs as keys
-    subjects_dict = {}
-    for semester in data:
-        for subject in semester:
-            subject_id = subject["id"]
-            subjects_dict[subject_id] = subject
-
-    # find semester with the most subjects
-    max_semester = max_semester = max(list(data), key=len)
-
-    table = [[0 for _ in range(len(max_semester))] for _ in range(len(max_semester))]
-    # try every possible permutation of subjects, and find the one with the shortest sum of distances of children
-    min_distance = float("inf")
-    min_permutation = None
-    for permutation in itertools.permutations(max_semester):
-        distance = 0
-        for i, subject in enumerate(permutation):
-            for child in subject["children"]:
-                child_index = [s["id"] for s in permutation].index(child)
-                distance += abs(i - child_index)
-        if distance < min_distance:
-            min_distance = distance
-            min_permutation = permutation
-
-
 def process_files(files_to_process) -> list[dict]:
     subjects = []
     for file_name, specialization, table_type in files_to_process:
-        new_data = parse_csv(file_name, specialization, table_type)
+        new_data = parse_csv(file_name, table_type)
         # add new data to the list of subjects, where subject ids are unique
         for subject in new_data:
             if subject["id"] not in [s["id"] for s in subjects]:
@@ -275,9 +172,6 @@ def main():
     subjects = process_files(files_to_process)
     map_parents_as_children(subjects)
     data = sort_into_semesters(subjects)
-    # brute_force_sort(subjects)
-    data = closest_permutation(data)
-    # sort_data(data)
     save_data(data)
 
 
